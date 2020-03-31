a105632c7d758d9d3ff5fcd0ae97e81fc8446631
Leo <hamlap6@gmail.com>
2020-03-30 23:04:36 -0700

    tidy appearance with css and make single target

--- project1/js/app.js
+++ project1/js/app.js
@@ -1,7 +1,7 @@
 $(() => {
 
 
-// on opening, have modal that displays story
+// on opening, have modal that displays intro
     const $introModal = $('#intro-modal');
     const $closeIntro = $('.close');
 
@@ -13,36 +13,87 @@ $(() => {
 // timer on top
     // simply counts down from variable time, which by default
     // will be a set amount of time, but can be adjusted at bottom
-    let time = 10;
+    let time = 5;
     const $roundTime = $('#time').text(time);
     
-    const countDownTime = setInterval(function() {
-        time --;
-        $roundTime.text(time);
-        if(time < 0){
-            clearInterval(countDownTime);
-            $roundTime.text('TIME IS UP');
-        }
-    }, 1000)
+    const countDownTime = () => {
+        const timer = setInterval(function() {
+            time --;
+            $roundTime.text(time)
+            if(time < 0){
+                $roundTime.css('font-size', '23px');
+                $roundTime.text('TIME IS UP');
+                clearInterval(timer);
+            }
+        }, 1000);
+    }
+    
+    $closeIntro.on('click', countDownTime);
 
-
-
-// game area
-    // math.random to randomly generate position x and position y
-    // divs that pop up are absolute and contain random images which will be in an array
-    // 2 different arrays to pick from, of good and bad
-    // target will last a set time before removing itself
-    // clicking on image will remove the div and add a point to score while decrementing target left
-    // clicking on body will decrement score
-    // after all the targets have been displayed and clicked, round is over
-    // once round is over, either display button or time before next round
 
 
 // info area
+    const $score = $('#score');
+    const $missCounter = $('#miss-counter');
+    const $remainingTargets = $('#remaining-targets');
+    const $round = $('#round');
+
+    let score = 0;
+    let missCounter = 0;
+    let remainingTargets = 20;
+    let round = 1;
     // score which will update continuously upon clicks
     // miss clicked which will track expired targets and if you click on body
     // targets left will be default 20 targets, but can be set in bottom
     // round count will be default 5, but can be adjusted
+    
+    const updateInfo = () => {
+        $score.text(score);
+        $missCounter.text(missCounter);
+        $remainingTargets.text(remainingTargets);
+        $roundTime.text(round);
+    }
+
+    
+// game area
+    // 2 different arrays to pick from, of good and bad
+    const pestArray = [
+        "https://cdn.pixabay.com/photo/2013/07/13/10/26/snail-157212_1280.png", "https://cdn.pixabay.com/photo/2012/04/02/14/24/hungry-worm-24634_1280.png", "https://cdn.pixabay.com/photo/2013/07/13/10/15/cockroach-156887_1280.png"  
+    ]
+
+    const dontClickArray = [
+        "https://cdn.pixabay.com/photo/2014/04/03/09/58/eggplant-309459_1280.png", "https://cdn.pixabay.com/photo/2012/04/18/20/21/strawberry-37781_1280.png", "https://cdn.pixabay.com/photo/2013/07/12/18/19/tomato-153272_1280.png"
+    ]
+
+    const $gamezone = $('#play-area');
+
+    const createTargets = () => {
+        const target = $('<div>').addClass('target');
+        target.css('background-image', `url(${pestArray[0]}`);
+
+        target.on('click', (event) => {
+            const item = event.currentTarget;
+            // clicking on image will remove the div and add a point to score while decrementing target left
+            // increment score and decrement targets left
+            score += 100;
+            remainingTargets --;
+            updateInfo();
+            item.remove();
+            
+        })
+        
+        $gamezone.append(target);
+    }
+    createTargets();
+
+
+    // math.random to randomly generate position x and position y
+    // divs that pop up are absolute and contain random images which will be in an array
+    // target will last a set time before removing itself
+    // clicking on body will decrement score
+    // after all the targets have been displayed and clicked, round is over
+    // once round is over, either display button or time before next round
+
 
 
 // option area
